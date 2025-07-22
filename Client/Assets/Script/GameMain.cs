6c09155281a088fb90f50a5c3bf4c64eb4c2448d
wangsongxue <wwwangsx@gmail.com>
2025-07-22 20:47:08 +0800

    GameMain

--- Client/Assets/Script/GameMain.cs
+++ Client/Assets/Script/GameMain.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class class1 : MonoBehaviour
+public class GameMain : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
