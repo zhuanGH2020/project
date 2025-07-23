f42f132fe5bf97e46395b950d6304b60bde5eed2
zhuanGH2020 <137578290@qq.com>
2025-07-23 15:28:31 +0800

    GameMain

--- Client/Assets/Script/GameMain.cs
+++ Client/Assets/Script/GameMain.cs
@@ -7,7 +7,7 @@ public class GameMain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("123GameMain Start");
     }
 
     // Update is called once per frame
