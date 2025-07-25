56567f163f166237bf8e7f6054bd5407fb566bf9
zhuanGH2020 <137578290@qq.com>
2025-07-25 22:24:30 +0800

    ui make

--- Client/Assets/Script/UI/UIDlg/MakeView.cs
+++ Client/Assets/Script/UI/UIDlg/MakeView.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class MakeView : MonoBehaviour
 {
@@ -11,51 +13,65 @@ public class MakeView : MonoBehaviour
     
     private void InitializeMakeList()
     {
-        Transform listMakeTransform = transform.Find("list_make");
-        if (listMakeTransform == null)
-        {
-            listMakeTransform = FindChildWithUIList();
-            if (listMakeTransform == null)
-                return;
-        }
+        UIList uiList = GetUIList();
+        if (uiList == null) return;
         
-        UIList uiList = listMakeTransform.GetComponent<UIList>();
-        if (uiList == null)
-        {
-            uiList = listMakeTransform.GetComponentInChildren<UIList>();
-            if (uiList == null)
-                return;
-        }
+        var reader = LoadMakeConfig();
+        if (reader == null) return;
         
-        if (!ConfigManager.Instance.LoadConfig("Make", "Configs/make"))
-            return;
-        
-        var reader = ConfigManager.Instance.GetReader("Make");
-        if (reader == null)
-            return;
-        
+        CreateMakeItems(uiList, reader);
+    }
+    
+    private UIList GetUIList()
+    {
+        Transform listTransform = transform.Find("list_make") ?? FindChildWithUIList();
+        return listTransform?.GetComponent<UIList>() ?? listTransform?.GetComponentInChildren<UIList>();
+    }
+    
+    private ConfigReader LoadMakeConfig()
+    {
+        if (!ConfigManager.Instance.LoadConfig("Make", "Configs/make")) return null;
+        return ConfigManager.Instance.GetReader("Make");
+    }
+    
+    private void CreateMakeItems(UIList uiList, ConfigReader reader)
+    {
         foreach (var id in reader.GetAllIds())
         {
-            uiList.AddListItem();
+            GameObject item = uiList.AddListItem();
+            if (item == null) continue;
+            
+            string name = reader.GetValue<string>(id, "Name", "");
+            
+            var txtName = item.transform.Find("txt_name")?.GetComponent<TextMeshProUGUI>();
+            if (txtName != null)
+            {
+                txtName.text = name;
+            }
+            
+            var toggle = item.GetComponent<Toggle>();
+            if (toggle != null)
+            {
+                string itemName = name;
+                toggle.onValueChanged.AddListener((isOn) => OnItemToggle(itemName, isOn));
+            }
         }
     }
     
+    private void OnItemToggle(string name, bool isOn)
+    {
+        Debug.Log($"Toggle item: {name}, isOn: {isOn}");
+    }
+
     private Transform FindChildWithUIList()
     {
         for (int i = 0; i < transform.childCount; i++)
         {
             Transform child = transform.GetChild(i);
-            UIList uiList = child.GetComponent<UIList>();
-            if (uiList != null)
-            {
-                return child;
-            }
+            if (child.GetComponent<UIList>() != null) return child;
             
-            uiList = child.GetComponentInChildren<UIList>();
-            if (uiList != null)
-            {
-                return uiList.transform;
-            }
+            var childUIList = child.GetComponentInChildren<UIList>();
+            if (childUIList != null) return childUIList.transform;
         }
         return null;
     }
