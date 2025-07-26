35d0de057dd45478a384cf509dddb4824476021e
zhuanGH2020 <137578290@qq.com>
2025-07-26 19:47:34 +0800

    enum

--- Client/Assets/Script/Core/Enums.cs
+++ Client/Assets/Script/Core/Enums.cs
@@ -13,29 +13,4 @@ public enum ItemType
     Survival = 3,    // 生存
     Tech = 4,        // 科技
     Equip = 5,       // 装备
-} 
-
-/// <summary>
-/// 怪物类型
-/// </summary>
-public enum MonsterType
-{
-    None = 0,
-    Normal = 1,    // 普通僵尸
-    Boss = 2,      // Boss僵尸
-    Poison = 3,    // 毒性僵尸
-    Tank = 4,      // 坦克僵尸
-}
-
-/// <summary>
-/// 伙伴类型
-/// </summary>
-public enum PartnerType
-{
-    None = 0,
-    Peashooter = 1,    // 豌豆射手
-    IceShooter = 2,    // 冰豌豆射手
-    FireShooter = 3,   // 火豌豆射手
-    LightShooter = 4,  // 光明射手
-    DarkShooter = 5,   // 黑暗射手
 }
