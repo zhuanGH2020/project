c74fd74cfec33b7ea2431d50848342825055cf24
zhuanGH2020 <137578290@qq.com>
2025-07-28 10:57:09 +0800

    move config_converter.vat

--- Client/Assets/Resources/Configs/config_converter.bat
+++ Client/Assets/Resources/Configs/config_converter.bat
@@ -4,8 +4,8 @@ REM 配置文件编码转换工具 - 反向转换版本
 REM 输入: Assets/Resources/Configs/ 目录
 REM 输出: CSV/ 目录
 
-set SRC_DIR=..\Assets\Resources\Configs
-set DST_DIR=..\CSV
+set SRC_DIR=.
+set DST_DIR=..\..\..\CSV
 
 echo 配置文件编码转换工具
 echo ================================================
@@ -17,7 +17,7 @@ echo.
 REM 检查输入目录是否存在
 if not exist "%SRC_DIR%" (
     echo 错误: 输入目录不存在 - %SRC_DIR%
-    echo 请确保项目目录下有Assets\Resources\Configs文件夹
+    echo 请确保当前目录是Assets\Resources\Configs文件夹
     pause
     exit /b 1
 )
@@ -29,14 +29,15 @@ if not exist "%DST_DIR%" (
 )
 
 echo 启动配置文件编码转换工具...
-config_converter\config_converter.exe "%SRC_DIR%" "%DST_DIR%"
+..\..\..\Tools\config_converter\config_converter.exe "%SRC_DIR%" "%DST_DIR%"
 
 if errorlevel 1 (
     echo.
     echo 转换过程中出现错误，请检查:
     echo 1. 输入目录 %SRC_DIR% 是否存在
     echo 2. 输出目录 %DST_DIR% 是否有写入权限
-    echo 3. 可执行文件 config_converter.exe 是否存在
+    echo 3. 可执行文件 ..\..\..\Tools\config_converter\config_converter.exe 是否存在
+    echo 4. 目标CSV文件是否被Excel或其他程序占用
     pause
     exit /b 1
 )
